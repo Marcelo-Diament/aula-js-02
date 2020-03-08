67b28bcc946747292faffd20c2b33a19e96ecd97
Marcelo-Diament <marcelodiament@gmail.com>
2020-03-08 19:25:52 -0300

    fix: few adjusts related to responsive behaviour

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body>
     <?php require_once("./inc/header.php"); ?>
     <main class="container my-5">
-    <article class="row">
+    <article class="row px-3 px-md-0">
         <h1 class="col-12">Práticas de JS</h1>
         <p class="col-12">Para realização da prática em aula, <a href="https://github.com/Marcelo-Diament/aula-js-02-pre">baixe o repositório</a> que preparamos para você!</p>
         <section class="col-12 bg-light rounded border p-5 my-3">
@@ -13,22 +13,24 @@
             <p>Acesse os links do menu para ver alguns conceitos básicos de JS em prática.</p>
             <p>Não se esqueça de abrir o console para ver alguns avisos, dicas e explicações extra</p>
         </section>
-        <section class="col-12 bg-light rounded border p-5 my-3 row">
+        <section class="col-12 bg-light rounded border p-5 my-3">
             <h2 class="col-12 mb-4">Conteúdo Abordado</h2>
-            <ul class="col-12 col-lg-6">
-                <li>console.log()</li>
-                <li>seletores</li>
-                <li>manipulação do DOM</li>
-                <li>funções</li>
-                <li>loops</li>
-            </ul>
-            <ul class="col-12 col-lg-6">
-                <li>condicionais</li>
-                <li>eventos</li>
-                <li>AJAX</li>
-                <li>callback</li>
-                <li>fetch API</li>
-            </ul>
+            <div class="col-12 row px-5">
+                <ul class="col-12 col-lg-6">
+                    <li>console.log()</li>
+                    <li>seletores</li>
+                    <li>manipulação do DOM</li>
+                    <li>funções</li>
+                    <li>loops</li>
+                </ul>
+                <ul class="col-12 col-lg-6">
+                    <li>condicionais</li>
+                    <li>eventos</li>
+                    <li>AJAX</li>
+                    <li>callback</li>
+                    <li>fetch API</li>
+                </ul>
+            </div>
         </section>
         <section class="col-12 bg-light rounded border p-5 my-3">
             <h2 class="mb-4">Repositório</h2>
